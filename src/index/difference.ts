9031a57194d182c601537fc7a30a882523b4ac8f
purple-comet <131944632+purple-comet@users.noreply.github.com>
2025-11-09 23:25:03 +0900

    refactor: ディレクトリ整理

--- src/index/difference.ts
+++ src/index/difference.ts
@@ -1,36 +1,11 @@
 import { type LoadedNotes, type NotesWithTime, type NoteType } from "./types.js";
+import { loadSimaiData } from "./loadSimaiData.js";
 
 function calculateDifference() {
-
+    
 }
 
-function loadSimaiData (data: string) {
-    // preprocess data: remove newlines and spaces
-    const onelineData = data
-    .replace(/(\r)?\n/g, '')
-    .replace(/\s+/g, '');
-    const bpmRegex = /\((\d+)\)([^(]+)/g;
-    const notesByBpm: LoadedNotes[] = [];
-    // extract notes grouped by BPM
-    for (const match of onelineData.matchAll(bpmRegex)) {
-        const rawNotes = match[2];
-        const beatRegex = /\{(\d+)\}([^{]+)/g;
-        const notesByBeat = [];
-        // extract notes grouped by beat
-        for (const beatMatch of rawNotes.matchAll(beatRegex)) {
-            notesByBeat.push({
-                beat: Number(beatMatch[1]),
-                notes: beatMatch[2].split(',').slice(0, -1), // remove last empty element
-            });
-        }
-        notesByBpm.push({
-            bpm: Number(match[1]),
-            notes: notesByBeat,
-            rawNotes: match[2],
-        });
-    }
-    return notesByBpm
-}
+
 export class Main {
     private button: HTMLButtonElement;
     private notes: any[] = [];
